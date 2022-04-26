0ffe98e64947ebbdf8fb931d94cc1664ae5457f4
zhouxx <mozu_2008@163.com>
2022-04-26 22:43:15 +0800

    add licence

--- boot-plus-web/src/main/java/com/alilitech/web/support/MessageResourceCollection.java
+++ boot-plus-web/src/main/java/com/alilitech/web/support/MessageResourceCollection.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2017-present the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.alilitech.web.support;
 
 import org.springframework.context.MessageSource;
@@ -17,7 +32,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class MessageResourceCollection {
 
-    public static MessageResourceCollection EMPTY = new MessageResourceCollection();
+    public static final MessageResourceCollection EMPTY = new MessageResourceCollection();
 
     private final Map<String, MessageSource> messageSourceMap = new ConcurrentHashMap<>();
 
